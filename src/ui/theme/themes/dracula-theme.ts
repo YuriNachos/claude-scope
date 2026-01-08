2cfe058a16887c333a0461ddf7017af8baf45438
Yurii Chukhlib <yurii.chukhlib@viber.com>
2026-01-09 00:03:11 +0100

    feat(theme): add Dracula theme

---
+++ src/ui/theme/themes/dracula-theme.ts
@@ -0,0 +1,28 @@
+/**
+ * Dracula theme
+ * Purple/pink accent theme
+ * https://draculatheme.com/
+ */
+
+import { rgb } from "../../utils/colors.js";
+import { createThemeColors } from "../helpers.js";
+import type { ITheme } from "../types.js";
+
+export const DRACULA_THEME: ITheme = {
+  name: "dracula",
+  description: "Purple/pink accent theme",
+
+  colors: createThemeColors({
+    branch: rgb(189, 147, 249), // Purple
+    changes: rgb(139, 233, 253), // Cyan
+    contextLow: rgb(80, 250, 123), // Green
+    contextMedium: rgb(241, 250, 140), // Yellow
+    contextHigh: rgb(255, 85, 85), // Red
+    linesAdded: rgb(80, 250, 123), // Green
+    linesRemoved: rgb(255, 85, 85), // Red
+    cost: rgb(255, 184, 108), // Orange
+    model: rgb(98, 114, 164), // Comment gray
+    duration: rgb(68, 71, 90), // Selection gray
+    accent: rgb(189, 147, 249), // Purple
+  }),
+};
