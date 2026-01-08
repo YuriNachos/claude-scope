211413189ffc7e051d730cfb668b916ccad9bd8e
Yurii Chukhlib <yurii.chukhlib@viber.com>
2026-01-08 19:21:21 +0100

    feat(config-count): add style renderer types

---
+++ src/widgets/config-count/renderers/types.ts
@@ -0,0 +1,14 @@
+/**
+ * Types for ConfigCountWidget style renderers
+ */
+
+import type { BaseStyleRenderer } from "../../../core/style-renderer.js";
+
+export interface ConfigCountRenderData {
+  claudeMdCount: number;
+  rulesCount: number;
+  mcpCount: number;
+  hooksCount: number;
+}
+
+export type ConfigCountRenderer = BaseStyleRenderer<ConfigCountRenderData>;
