aedc395836698a380bc04f73943b1fd613e8ea02
Yurii Chukhlib <yurii.chukhlib@viber.com>
2026-01-12 14:12:10 +0100

    feat(dev-server): add index export

---
+++ src/widgets/dev-server/index.ts
@@ -0,0 +1,6 @@
+/**
+ * DevServerWidget - Detects running dev server processes
+ */
+
+export { DevServerWidget } from "./dev-server-widget.js";
+export type { DevServerRenderData, DevServerStatus, ProcessPattern } from "./types.js";
